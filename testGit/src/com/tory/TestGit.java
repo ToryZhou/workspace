b00b710def83b08c067ef1d44aaeb355cd410ec2
Administrator <Administrator@Tory-PC>
2014-10-21 13:21:53 +0800

    add a folder

---
+++ testGit/src/com/tory/TestGit.java
@@ -0,0 +1,14 @@
+/**	______	    		
+   /__	__/__  ____     
+ 	 / / / _ \/ __) _ `/
+ 	/_/ ()___/_/  \_, /
+ 				 /___/			
+ * ©2014-10-21 上午11:46:51 snail
+ */
+package com.tory;
+
+public class TestGit {
+	public static void main(String[] args) {
+		System.out.println("hello git!");
+	}
+}
