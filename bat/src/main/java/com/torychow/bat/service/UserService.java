51cc0355a5d36fd31dd837fde3f1b3fc4b11ec00
Administrator <Administrator@Tory-PC>
2014-10-23 17:45:12 +0800

    Signed-off-by: Administrator <Administrator@Tory-PC>

--- bat/src/main/java/com/torychow/bat/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.torychow.bat.service;
-
-
-
-
-public interface UserService {
-	public void register(String sUserName, String sUserEmail,
-			String sUserPassword, String sUserPasswordConfirm);
-}
